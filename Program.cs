9ee77b33bc73f684ff6da47b09038951ef20cbf5
Scott McIntosh <v-mimci@microsoft.com>
2021-06-25 16:42:11 -0700

    Changed file pattern name for splits

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace Base64FileSplitter
 {
 	class Program
 	{
+		static string FilePattern { get; set; }
+
 		static int Main(string[] args)
 		{
 			try
@@ -21,6 +23,7 @@ namespace Base64FileSplitter
 				}
 				else
 				{
+					FilePattern = GetFilePattern(o.CurrentFile);
 					SplitFileUsingOptions(o);
 				}
 			});
@@ -31,17 +34,23 @@ namespace Base64FileSplitter
 				WriteErrorToConsole(ex.ToString());
 			}
             return 0;
-        }
+         }
+
+		private static string GetFilePattern(string currentFile)
+		{
+			var fileInfo = new FileInfo(currentFile);
+			var extensionReplacement = fileInfo.Name.Replace($"{fileInfo.Extension}", $"-.part<<INDEX>>");
+			return Path.Combine(fileInfo.Directory.FullName, extensionReplacement);
+		}
 
 		private static void SplitFileUsingOptions(Options o)
 		{
 			var splitter = new FileSplitter(o.CurrentFile, o.BlockSize);
-			var fileInfo = new FileInfo(o.CurrentFile);
 			WriteMessageToConsole("Splitting file...");
 			var splitParts = splitter.SplitFile();
 			Parallel.ForEach(splitParts, async (part) =>
 			{
-				var fileName = Path.Combine(fileInfo.Directory.FullName, fileInfo.Name.Replace(".", $"-{part.Number}."));
+				var fileName = FilePattern.Replace("<<INDEX>>", part.Number.ToString());
 				await File.WriteAllTextAsync(fileName, part.Base64Fragment);
 			});
 			WriteMessageToConsole($"File was split into {splitParts.Count()} parts");
